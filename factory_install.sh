95faf82048e1c86e1c74c93accb428572c7b6c4c
TheTechFrog <96459640+TheSpiritOfDark@users.noreply.github.com>
2024-03-13 12:30:29 -0500

    add checking for reco images
    I copied this from the shim section lol, thx Sean

--- factory_install.sh
+++ factory_install.sh
@@ -82,7 +82,17 @@ shimboot() {
 }
 
 installcros() {
-	echo "Not implemented yet."
+	crosmidi_images="$(cgpt find -l CROSMIDI_IMAGES | head -n 1 | grep --color=never /dev/)"
+	mount $crosmidi_images /mnt/crosmidi
+	find /mnt/crosmidi/recovery -type f
+	while true; do
+ 		echo "NOT IMPLIMENTED, Images will not flash, type 'exit' to return to the crosmidi menu."
+		read -p "Please choose an image to recover with: " shimtoboot
+		
+		if [[ $shimtoboot == "exit" ]]
+		then
+			break
+		fi
 }
 
 rebootdevice() {
